f012ce5703c3a40d4171774e6739baffc451feeb
Alex Mozerski <alex.mozerski@he-arc.ch>
2022-03-09 08:58:47 +0100

    add * in host

--- historiaproject/historiaproject/settings.py
+++ historiaproject/historiaproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f_^!&yijzdedjb0h+g*dy@+!6gf_oka7n)8yhwjf9dq&_@3t86
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
