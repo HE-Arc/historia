8f1fc449d363ed7f2405da338c5817e6e3a6ae4c
Yasmine Margueron <yasmine.marguero@hes-so.ch>
2022-03-31 13:56:47 +0200

    Clean model

--- historiaproject/historiaapp/models.py
+++ historiaproject/historiaapp/models.py
@@ -22,15 +22,7 @@ class AnswerOptions(models.Model):
     question_id = models.ForeignKey('Question', on_delete=models.CASCADE)
     answerText = models.CharField(max_length=200)
     isCorrect = models.BooleanField(default=False)
-
-class Score(models.Model):
-    #user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    quiz_id = models.ForeignKey('Quiz', on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    start_time = models.TimeField(auto_now_add=True)
-    end_time = models.TimeField(auto_now_add=True)
-
-    
+   
 class Answer(models.Model):
     score_id = models.ManyToManyField('Score')
     answer_option_id = models.ManyToManyField('AnswerOptions')
@@ -110,13 +102,10 @@ class Quiz(models.Model):
 class Ranking(models.Model):
     # quiz
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    
     # user
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
     # score
     score = models.IntegerField()
-    
     # date
     date = models.DateField(db_index=True)
     
