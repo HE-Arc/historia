af83bfc350bd0897d6571bb99a296dbe4d736b6e
Simon Meier <simon.meier@he-arc.ch>
2022-04-07 19:50:06 +0200

    o_o

--- historiaproject/historiaproject/settings.py
+++ historiaproject/historiaproject/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'historiaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'historia',
         'USER': 'postgres',
         'PASSWORD': 'root',
