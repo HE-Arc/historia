a1bce7a9ec398fa23c40e35ebe0d67d5f7403f65
Yasmine Margueron <yasmine.marguero@hes-so.ch>
2022-04-10 13:49:19 +0200

    Modify settings to work in local

--- historiaproject/historiaproject/settings.py
+++ historiaproject/historiaproject/settings.py
@@ -87,24 +87,19 @@ WSGI_APPLICATION = 'historiaproject.wsgi.application'
 # 'NAME': BASE_DIR / 'db.sqlite3',
 
 DATABASES = {
-
     'default': {
-        '''
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3'
-        '''
         
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'historia',
-        'USER': os.environ.get('GROUPNAME'),
-        'PASSWORD': os.environ.get('PASSWORD', ''),
-        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-        'PORT': os.environ.get('MYSQL_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'ssl_mode' : 'DISABLED'
-        }
-        
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'historia',
+        # 'USER': os.environ.get('GROUPNAME'),
+        # 'PASSWORD': os.environ.get('PASSWORD', ''),
+        # 'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        # 'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        # 'OPTIONS': {
+        #     'charset': 'utf8mb4'
+        # }
     }
 }
 
