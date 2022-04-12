50675b4b8f8e96ce9fcad2aef51f67167262aa27
Simon Meier <simon.meier@he-arc.ch>
2022-04-13 01:27:18 +0200

    Changing the settings.py. Adapating to mysql database.

--- historiaproject/historiaproject/settings.py
+++ historiaproject/historiaproject/settings.py
@@ -90,17 +90,15 @@ WSGI_APPLICATION = 'historiaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'historia',
-        # 'USER': os.environ.get('GROUPNAME'),
-        # 'PASSWORD': os.environ.get('PASSWORD', ''),
-        # 'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-        # 'PORT': os.environ.get('MYSQL_PORT', '3306'),
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4'
-        # }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'historia',
+        'USER': os.environ.get('GROUPNAME'),
+        'PASSWORD': os.environ.get('PASSWORD', ''),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
     }
 }
 
